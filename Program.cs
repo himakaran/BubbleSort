f9acce7fe330009a7fbd4083cd0a9317f31b7cd6
Karan <himakaran@usf.edu>
2024-02-28 21:47:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            int[] arr = { 5, 72, 3, 14, 7, 1 };
+            BubbleSort(arr);
+            Console.WriteLine("Sorted array:");
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+        }
+
+        static void BubbleSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
